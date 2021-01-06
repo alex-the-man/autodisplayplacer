f0187790cef1e7e68caddab860f17a5723f48122
Alex Man <alex-the-man@users.noreply.github.com>
2021-01-05 19:58:00 -0800

    Version 0.1

---
+++ autodisplayplacer/AppDelegate.swift
@@ -0,0 +1,121 @@
+//
+//  AppDelegate.swift
+//  autodisplayplacer
+//
+//  Created by Alex Man on 1/5/21.
+//
+
+import Cocoa
+
+// These are used in Storyboard bindings. Do not change.
+struct ConfigKeys {
+    static let executeCmdOnStart = "executeCmdOnStart"
+    static let cmd = "cmd"
+}
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    @IBOutlet weak var statusBarMenu: NSMenu?
+    
+    let displayChangeQueue = DispatchQueue(label: "DisplayChangeQueue")
+    let userDefaults: UserDefaults
+    let storyboard = NSStoryboard(name: "Main", bundle: nil)
+    
+    lazy var preferencesViewController = storyboard.instantiateController(withIdentifier: .init(stringLiteral: "preferencesID")) as! NSWindowController
+    
+    var prevDisplayCount: UInt32 = getDisplayCount()
+    var statusItem: NSStatusItem?
+    
+    override init() {
+        NSUserDefaultsController.shared.appliesImmediately = false
+
+        userDefaults = UserDefaults.standard
+        userDefaults.register(
+            defaults: [
+                ConfigKeys.executeCmdOnStart: true,
+                ConfigKeys.cmd: "say \"Screen change detected\"",
+            ]
+        )
+    }
+    
+    func applicationDidFinishLaunching(_ aNotification:  Notification) {
+        createStatusBarIcon()
+        
+        NSLog("Initial display count: %d", prevDisplayCount)
+        if userDefaults.bool(forKey: ConfigKeys.executeCmdOnStart) {
+            self.executeConfiguredCmd(force: true)
+        }
+    }
+    
+    func createStatusBarIcon() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        
+        let statusBarIconImage = NSImage(named: "StatusBarIcon")!
+        statusItem!.button?.image = statusBarIconImage
+        statusItem!.menu = statusBarMenu
+    }
+    
+    func applicationDidChangeScreenParameters(_ notification: Notification) {
+        self.executeConfiguredCmd(force: false)
+    }
+    
+    func executeConfiguredCmd(force: Bool) {
+        displayChangeQueue.async {
+            let displayCount = getDisplayCount()
+            guard force || self.prevDisplayCount != displayCount else {
+                NSLog("Display parameters changed but the number of displays hasn't changed. Noop. Current display count: %d", displayCount)
+                return
+            } 
+            guard let cmd = self.userDefaults.string(forKey: ConfigKeys.cmd),
+                  !cmd.isEmpty else {
+                NSLog("User provided command is empty.")
+                return
+            }
+                
+            NSLog("New display count: %u. Force: %@, Calling command: %@", displayCount, force.description, cmd)
+            let cmdOutput = executeCommand(cmd)
+            NSLog("Command output: %@", cmdOutput)
+            
+            self.prevDisplayCount = displayCount
+        }
+    }
+    
+    @IBAction func executCmdManually(_ sender: Any) {
+        executeConfiguredCmd(force: true)
+    }
+    
+    @IBAction func showPreferences(_ sender: Any) {
+        preferencesViewController.loadWindow()
+        let preferencesWindow = preferencesViewController.window!
+        if !preferencesWindow.isVisible {
+            preferencesWindow.contentViewController?.representedObject = AppPreferences(command: userDefaults.string(forKey: ConfigKeys.cmd)!)
+        }
+        preferencesWindow.makeKeyAndOrderFront(nil)
+        NSApplication.shared.activate(ignoringOtherApps: true)
+    }
+}
+
+func executeCommand(_ command: String) -> String {
+    let stdouterr = Pipe()
+    let task = Process()
+    task.launchPath = "/bin/bash" // Use bash to parse the command line.
+    task.arguments = ["-c", command]
+    task.standardOutput = stdouterr
+    task.standardError = stdouterr
+    task.launch()
+    
+    // TODO Change it to timed wait.
+    task.waitUntilExit()
+    
+    _ = try? stdouterr.fileHandleForWriting.close()
+    let data = stdouterr.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: String.Encoding.utf8)!
+    
+    return output
+}
+
+func getDisplayCount() -> UInt32 {
+    var displayCount: UInt32 = 0
+    CGGetOnlineDisplayList(UInt32.max, nil, &displayCount)
+    return displayCount
+}
---
+++ autodisplayplacer/PreferencesViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  autodisplayplacer
+//
+//  Created by Alex Man on 1/5/21.
+//
+
+import Cocoa
+
+public class AppPreferences: NSObject {
+    @objc dynamic var command: String
+
+    public init(command: String) {
+        self.command = command
+    }
+}
+
+class PreferencesViewController: NSViewController {
+    @IBOutlet var commandTextView: NSTextView!
+    @IBOutlet var executeButton: NSButton!
+    @IBOutlet var outputTextView: NSTextView!
+    @IBOutlet var progressIndicator: NSProgressIndicator!
+        
+    override func viewDidLoad() {
+        self.commandTextView.isAutomaticQuoteSubstitutionEnabled = false;
+        self.commandTextView.isAutomaticDashSubstitutionEnabled = false;
+        self.commandTextView.isAutomaticTextReplacementEnabled = false;
+    }
+    
+    override func viewDidDisappear() {
+        NSUserDefaultsController.shared.revert(nil)
+    }
+
+    @IBAction func execute(_ sender: Any) {
+        self.progressIndicator.startAnimation(nil)
+        let cmd = self.commandTextView.string
+        self.outputTextView.string = ""
+        DispatchQueue.global(qos: .background).async {
+            let output = executeCommand(cmd)
+            DispatchQueue.main.async {
+                self.outputTextView.string = output.isEmpty ? "<no output>" : output
+                self.progressIndicator.stopAnimation(nil)
+            }
+        }
+    }
+}
