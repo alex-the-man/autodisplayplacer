08800067d46041440a1eadeafa6e5fd91e410443
Alex Man <alex-the-man@users.noreply.github.com>
2021-01-08 18:33:12 -0800

    Add dmg build script.

---
+++ build-dmg.sh
@@ -0,0 +1,10 @@
+create-dmg \
+  --volname "AutoDisplayPlacer Installer" \
+  --window-pos 200 120 \
+  --window-size 800 400 \
+  --icon-size 100 \
+  --icon "AutoDisplayPlacer.app" 200 190 \
+  --hide-extension "AutoDisplayPlacer.app" \
+  --app-drop-link 600 185 \
+  "AutoDisplayPlacer-Installer.dmg" \
+  "build/"
